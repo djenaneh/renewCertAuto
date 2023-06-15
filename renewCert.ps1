93907a518e486107d7164cbbe7812841b03ba4d7
Djenane Admin <djenane@herrod.xyz>
2023-06-15 23:10:55 +0000

    adding procuedure

--- renewCert.ps1
+++ renewCert.ps1
@@ -9,3 +9,12 @@ $subjectName="CN=mail.herrod.xyz"
 $path
 
 New-ExchangeCertificate -GenerateRequest -SubjectName $subjectName -PrivateKeyExportable $true -KeySize 2048 -Path "C:\certificates\mail.contoso.com.csr"
+
+#Submit the certificate request to a certification authority (CA) to obtain a new certificate. You can use a third-party CA or an internal CA[1].
+
+#Import the new certificate by running the command 
+Import-ExchangeCertificate -Path "C:\certificates\mail.contoso.com.cer" | Enable-ExchangeCertificate -Services "SMTP, IMAP, POP, IIS" 
+#in the Exchange Management Shell. Replace "C:\certificates\mail.contoso.com.cer" with the path to your new certificate file[1].
+
+#Verify that the new certificate is enabled for the correct services by running the command 
+Get-ExchangeCertificate | fl 
